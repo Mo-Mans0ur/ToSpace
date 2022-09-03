c38154f2379a4270c953c852846239151cd611dd
Mo-Mans0ur <Moha1998@outlook.dk>
2022-09-03 15:35:04 +0200

    made the earth move

---
+++ src/test/java/com/example/tospace2/ToSpace2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tospace2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToSpace2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
